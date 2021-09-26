6b42d1bfa803b06cb1befde2725513339dc54796
Luiz Faleiros <luiz.faleiros@iteris.com.br>
2021-09-26 12:06:07 -0300

    initial prokect

---
+++ src/PageRouter.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+const PageRouter = () => {
+    return (<Router>
+        <Switch>
+            <Route></Route>
+        </Switch>
+    </Router>);
+}
+
+export default PageRouter;
---
+++ src/auth/login/Login.tsx
@@ -0,0 +1,11 @@
+import React, {FunctionComponent} from 'react';
+
+interface LoginProps {
+    
+}
+ 
+const Login: FunctionComponent<LoginProps> = () => {
+    return ( <div>Login</div> );
+}
+ 
+export default Login;
